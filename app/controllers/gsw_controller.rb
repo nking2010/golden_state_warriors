0328d9a16cee420cbce2c8039b83b13e230933c3
John Arevalo <john.arevalo11@gmail.com>
2016-11-11 15:06:13 -0500

    modified view and controller

--- app/controllers/gsw_controller.rb
+++ app/controllers/gsw_controller.rb
@@ -1,7 +1,8 @@
 class GswController < ApplicationController
   def index
     @auth = {:username => 'nking2010', :password => 'password'}
-    @profile = HTTParty.get("https://www.mysportsfeeds.com/api/feed/pull/nba/2016-2017-regular/roster_players.json?fordate=20161111&", :basic_auth => @auth).parsed_response
+    @body = HTTParty.get("https://www.mysportsfeeds.com/api/feed/pull/nba/2016-2017-regular/roster_players.json?fordate=20161111&", :basic_auth => @auth).parsed_response
+    @players = @body["rosterplayers"]["playerentry"]
   end
 end
 
