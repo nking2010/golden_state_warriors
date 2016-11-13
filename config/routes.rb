36c952ee75fb61dec9ab3b3cec0e1169877c10c3
Nigell <nigellking@gmail.com>
2016-11-13 15:22:28 -0500

    Adding the rest of the files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get "/" => 'gsw#homepg'
   get "/home" => 'gsw#home'
   get '/:id' => 'gsw#index'
-=======
-
-  get "/" => 'gsw#homepg'
-  get "/home" => 'gsw#home'
-  get '/:id' => 'gsw#index'
-
-
->>>>>>> f9d6a6c783fd8d4eebb4c93054e4c7bfa8439973
 end
