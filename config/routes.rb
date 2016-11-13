f9d6a6c783fd8d4eebb4c93054e4c7bfa8439973
John Arevalo <john.arevalo11@gmail.com>
2016-11-13 15:12:10 -0500

    modified player links & routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # get '/' => 'gsw#home'
-  get '/:id' => 'gsw#index'
+
   get "/" => 'gsw#homepg'
-  # get '/:id' =>'gsw#show'
-  # get 'gsw/index'
-  # root 'gsw#index'
-  # match '/CHI', to: 'gsw#CHI', via: 'get'
-  #you can not forget the root!
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/home" => 'gsw#home'
+  get '/:id' => 'gsw#index'
+
+
 end
